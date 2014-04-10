4a7bb62f94cc136ed344013dccff1f53bd75a98b
vndrwtr2 <vndrwtr2@gmail.com>
2014-04-10 17:08:09 -0500

    4/10 Update
    Added GameObject class
    Changed Player to child of GameObject

--- Map.rb
+++ Map.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 require 'gosu'
-
+#require_relative 'Sprite'
 include Gosu
 
 module Tiles
@@ -8,18 +8,18 @@ module Tiles
 end
 
 class Map
-  attr_reader :width, :height
+  attr_reader :width, :height, :tilesize
   def initialize(window, filename)
     #Loads 20x20 pix tileset
     @tileset = Image.load_tiles(window, "media/Test Tileset.png", 20, 20, true)
-    
+    @tilesize = 15
     #Reads map.txt line by line and turns it into tiles
     lines = File.readlines(filename).map { |line| line.chomp }
     @height = lines.size
     @width = lines[0].size
     @tiles = Array.new(@width) do |x|
       Array.new(@height) do |y|
-        case lines[y][x, 1]
+        case lines[y][x]
           when '#'
             Tiles::Wall
           else
@@ -35,31 +35,104 @@ class Map
       @width.times do|x|
         tile = @tiles[x][y]
         if tile
-          @tileset[tile].draw(x*15, y*15, 0)
+          @tileset[tile].draw(x*@tilesize, y*@tilesize, 0)
         end
       end
     end
   end
 
   def solid?(x, y)
-    @tiles[x/15, y/15]
+    @tiles[x/@tilesize][y/@tilesize]
+  end
+end
+
+class GameObject
+  attr_reader :x, :y
+  def initialize(window, x, y, image)
+    @x, @y = x, y
+    @map = window.map
+    @tilesize = window.map.tilesize
+  end
+  #Checks Top right, Top left, Bottom right, and Bottom left corners for collision
+  def isValid?(newx,newy)
+    not @map.solid?(@x+newx, @y+newy)
+  end
+  def warp(newx, newy)
+    @x, @y = newx, newy
+  end
+  def update(vx,vy)
+    if vx > 0
+      vx.times { if isValid?(vx+1,vy); @x+=vx; end}
+    end
+    if vx < 0
+      (-vx).times { if isValid?(vx-1,vy); @x+=vx; end}
+    end
+    if vy > 0
+      vy.times { if isValid?(vx,vy-2); @y-=vy; end}
+    end
+    if vy < 0
+      (-vy).times { if isValid?(vx,vy+2); @y-=vy; end}
+    end
+  end
+end
+
+class Player < GameObject
+  def initialize(window, x, y, image)
+    super
+    @image, @image2, @image3, @image4 = *Image.load_tiles(window, image, 15, 15, false)
+  end
+  def draw()
+    @image.draw(@x, @y, 1, 1.0, 1.0)
+  end
+  def fail?(player_x, player_y, ghost_x, ghost_y)
+    if player_x == ghost_x && player_y == ghost_y
+      
+    end
+  end
+end
+
+class Ghost < GameObject
+  def initialize(window, x, y, image)
+    super
+    @image, @image2, @image3, @image4 = *Image.load_tiles(window, image, 15, 15, false)
+  end
+    def draw()
+    @image.draw(@x, @y, 1, 1.0, 1.0)
   end
 end
 
 class Game < Window
   attr_reader :map
   def initialize
-    super 640, 480, false
+    super 720, 540, false
     self.caption = "Honors Project"
     @map = Map.new(self, "media/Test Map.txt")
     @background = Image.new(self, "media/Space.png", true)
+    @player = Player.new(self, 255, 360, "media/Test Sprite.png")
+    @ghost = Ghost.new(self, 180, 180, "media/Test Sprite Enemy.png")
   end
 
   def draw
     @background.draw 0,0,0
     @map.draw
+    @player.draw
+    @ghost.draw
   end
+  
+  def update
+    vx = vy = 0
+    vx += 1 if button_down? KbRight
+    vx -= 1 if button_down? KbLeft
+    vy += 1 if button_down? KbUp
+    vy -= 1 if button_down? KbDown
+    @player.update(vx, vy)  
+    end
 end
+
+
+#  def button_down(id)
+#    if id == KbEscape then close end
+#  end
 
 window = Game.new
 window.show
