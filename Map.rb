0508faf32e99c612b48e9dc61c0d77ccadd826c7
mzheng6 <mzheng6@illinois.edu>
2014-04-17 16:19:22 -0500

    changes to ghost and update

--- Map.rb
+++ Map.rb
@@ -52,6 +52,7 @@ class GameObject
     @x, @y = x, y
     @map = window.map
     @tilesize = window.map.tilesize
+    @dir = 3
   end
   #Checks Top right, Top left, Bottom right, and Bottom left corners for collision
   def isValid?(newx,newy)
@@ -74,6 +75,15 @@ class GameObject
       (-vy).times { if isValid?(vx,vy+2); @y-=vy; end}
     end
   end
+  def getcoord()
+    return [@x, @y]
+  end
+  def getdir()
+    return @dir
+  end
+  def setdir(bloop) #whichever direction you set it to
+    @dir = bloop
+  end
 end
 
 class Player < GameObject
@@ -95,10 +105,21 @@ class Ghost < GameObject
   def initialize(window, x, y, image)
     super
     @image, @image2, @image3, @image4 = *Image.load_tiles(window, image, 15, 15, false)
+    @count = 0
   end
-    def draw()
+  def draw()
     @image.draw(@x, @y, 1, 1.0, 1.0)
   end
+  def getcount()
+    return @count
+  end
+  def addcount()
+    @count += 1
+  end
+  def clearcount()
+    @count = 0
+  end
+  
 end
 
 class Game < Window
@@ -106,10 +127,10 @@ class Game < Window
   def initialize
     super 720, 540, false
     self.caption = "Honors Project"
-    @map = Map.new(self, "media/Test Map.txt")
+    @map = Map.new(self, "media/map2.txt")
     @background = Image.new(self, "media/Space.png", true)
     @player = Player.new(self, 255, 360, "media/Test Sprite.png")
-    @ghost = Ghost.new(self, 180, 180, "media/Test Sprite Enemy.png")
+    @ghost = Ghost.new(self, 100, 227, "media/Test Sprite Enemy.png")
   end
 
   def draw
@@ -118,17 +139,73 @@ class Game < Window
     @player.draw
     @ghost.draw
   end
-  
+
   def update
     vx = vy = 0
+    
     vx += 1 if button_down? KbRight
     vx -= 1 if button_down? KbLeft
     vy += 1 if button_down? KbUp
     vy -= 1 if button_down? KbDown
-    @player.update(vx, vy)  
+    @player.update(vx, vy)
+    
+    dir = @ghost.getdir()
+    ghostcoord = @ghost.getcoord()
+    gx = ghostcoord[0]
+    gy = ghostcoord[1]
+    if @ghost.getcount() != 0
+      @ghost.addcount()
+      if @ghost.getcount() == 16
+        @ghost.clearcount()
+      end
+      if dir == 3
+        @ghost.update(1, 0) #going to the right
+      elsif dir == 2
+        @ghost.update(-1, 0) #going to the left
+      elsif dir == 0
+        @ghost.update(0, -1) #take it back now yall (down)
+      elsif dir == 1
+        @ghost.update(0, 1) #going up
+      else 
+        @ghost.update(0, 0) 
+      end
+      @ghost.setdir(dir)
+      return
     end
-end
+    posdir= Array.new
+    if @ghost.isValid?(gx + 15, gy) && dir !=2
+      posdir.push(3)  
+    end
+    if @ghost.isValid?(gx-1, gy) && dir != 3
+      posdir.push(2)
+    end
+    if @ghost.isValid?(gx, gy+15) && dir !=1
+      posdir.push(0)
+    end
+    if @ghost.isValid?(gx, gy-1) && dir != 0
+      posdir.push(1)
+    end
 
+    tempdir = posdir.sample
+    if tempdir == 3
+      @ghost.update(1, 0) #going to the right
+    elsif tempdir == 2
+      @ghost.update(-1, 0) #going to the left
+    elsif tempdir == 0
+      @ghost.update(0, -1) #take it back now yall (down)
+    elsif tempdir == 1
+      @ghost.update(0, 1) #going up
+    else 
+      @ghost.update(0, 0) 
+    end
+    @ghost.setdir(tempdir)
+    if tempdir != dir
+      @ghost.addcount()
+    end
+          #puts playercoord
+    
+  end
+end
 
 #  def button_down(id)
 #    if id == KbEscape then close end
