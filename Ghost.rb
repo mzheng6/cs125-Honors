306cde3cfa138ffa3780a5418c7d4190e097d237
mzheng6 <mzheng6@illinois.edu>
2014-04-17 16:33:05 -0500

    Testing this out
    Testing testing 1 2 3

--- Ghost.rb
+++ Ghost.rb
@@ -1,58 +1,75 @@
-#contains code for the Ghost class for this project
-class Ghost
-  def initialize(x, y, color, ai = AI.new)
-    @x = x
-    @y = y
-    @color = color
-    @ai = ai
+class Ghost < GameObject
+  def initialize(window, x, y, image)
+    super
+    @image, @image2, @image3, @image4 = *Image.load_tiles(window, image, 15, 15, false)
+    @count = 0
+  end
+  def draw()
+    @image.draw(@x, @y, 1, 1.0, 1.0)
+  end
+  def getcount()
+    return @count
+  end
+  def addcount()
+    @count += 1
+  end
+  def clearcount()
+    @count = 0
   end
   
-  def getX
-    return @x
-  end
-  
-  def getY
-    return @y
-  end
-
-  def getColor
-    return color
-  end
-  
-  def setColor(color)
-    @color = color
-  end
-  
-  def move #returns the new coordinates of the Ghost
-    @x, @y =* ai.move(getSurrounding)
-    return [@x, @y]
-  end
-  
-  def getSurrounding
-    array = [] #Main.getBoard will return true if it's not a wall
-    if(Main.getBoard(@x, @y + 1))
-      array.push([@x, @y+1])
-    end
-    if(Main.getBoard(@x + 1, @y + 1))
-      array.push([@X + 1, @y + 1])
-    end
-    if(Main.getBoard(@x +1, @y))
-      array.push([@X + 1, @y])
-    end
-    if(Main.getBoard(@x + 1, @y - 1))
-      array.push([@x + 1, @y -1])
-    end
-    if(Main.getBoard(@x, @y - 1))
-      array.push([@x, @y -1])
-    end
-    if(Main.getBoard(@x -1, @y -1))
-      array.push([@x - 1, @y - 1])
-    end
-    if(Main.getBoard(@x - 1, @y))
-      array.push([@x - 1, @y])
-    end
-    if(Main.getBoard(@x - 1, @y + 1))
-      array.push([@x - 1, @y + 1])
-    end
-  end
 end
+
+
+dir = @ghost.getdir()
+    ghostcoord = @ghost.getcoord()
+    gx = ghostcoord[0]
+    gy = ghostcoord[1]
+    if @ghost.getcount() != 0
+      @ghost.addcount()
+      if @ghost.getcount() == 16
+        @ghost.clearcount()
+      end
+      if dir == 3
+        @ghost.update(1, 0) #going to the right
+      elsif dir == 2
+        @ghost.update(-1, 0) #going to the left
+      elsif dir == 0
+        @ghost.update(0, -1) #take it back now yall (down)
+      elsif dir == 1
+        @ghost.update(0, 1) #going up
+      else 
+        @ghost.update(0, 0) 
+      end
+      @ghost.setdir(dir)
+      return
+    end
+    posdir= Array.new
+    if @ghost.isValid?(gx + 15, gy) && dir !=2
+      posdir.push(3)  
+    end
+    if @ghost.isValid?(gx-1, gy) && dir != 3
+      posdir.push(2)
+    end
+    if @ghost.isValid?(gx, gy+15) && dir !=1
+      posdir.push(0)
+    end
+    if @ghost.isValid?(gx, gy-1) && dir != 0
+      posdir.push(1)
+    end
+
+    tempdir = posdir.sample
+    if tempdir == 3
+      @ghost.update(1, 0) #going to the right
+    elsif tempdir == 2
+      @ghost.update(-1, 0) #going to the left
+    elsif tempdir == 0
+      @ghost.update(0, -1) #take it back now yall (down)
+    elsif tempdir == 1
+      @ghost.update(0, 1) #going up
+    else 
+      @ghost.update(0, 0) 
+    end
+    @ghost.setdir(tempdir)
+    if tempdir != dir
+      @ghost.addcount()
+    end
